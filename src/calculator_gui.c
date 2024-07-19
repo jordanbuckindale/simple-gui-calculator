4a03f7273e354d4fdf0894ae40b6ed3e04eb094e
Jordan Buckindale <94644161+jordanbuckindale@users.noreply.github.com>
2024-07-18 21:38:52 -0700

    Create calculator_gui.c

---
+++ src/calculator_gui.c
@@ -0,0 +1,9 @@
+/* 
+-----------------------------------------------------
+Jordan Buckindale
+Date: July 18 2024
+
+This program is a simple GUI calculator built in C using the GTK library.
+The goal of this project is to practice working with GUI elements and the GTK library, and to implement a GUI in a roulette C program.
+-----------------------------------------------------
+*/
